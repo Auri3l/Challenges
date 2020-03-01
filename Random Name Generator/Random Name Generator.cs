34071df35ff705e1fe5a05023ecb61f30e9b21fb
Ata Yiğit Telli <ytelli@gmail.com>
2020-03-01 17:19:42 +0300

    Random Name Generator Committed, issues with non-latin

---
+++ Random Name Generator/Random Name Generator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RandomNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            string[] name = { "Ata", "Yiğit", "Öbürü" };
+            string[] surname = { "Telli", "Değil", "Başka" };
+            Console.WriteLine(name[rand.Next(0, name.Length)] + " " + surname[rand.Next(0, surname.Length)]);
+        }
+    }
+}
