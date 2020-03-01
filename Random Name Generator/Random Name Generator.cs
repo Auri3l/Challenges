ac7df69037a3f87dabfb3905796fe6f7c0b2f25f
Ata Yiğit Telli <ytelli@gmail.com>
2020-03-01 17:23:44 +0300

    Added name & surname lists.

--- Random Name Generator/Random Name Generator.cs
+++ Random Name Generator/Random Name Generator.cs
@@ -7,9 +7,9 @@ namespace RandomNameGenerator
         static void Main(string[] args)
         {
             Random rand = new Random();
-            string[] name = { "Ata", "Yiğit", "Öbürü" };
-            string[] surname = { "Telli", "Değil", "Başka" };
-            Console.WriteLine(name[rand.Next(0, name.Length)] + " " + surname[rand.Next(0, surname.Length)]);
+            string[] name = System.IO.File.ReadAllLines(@"C:\Users\Ata Yiğit TELLİ\Source\Repos\Challenges\Random Name Generator\Namelists\isimler.txt");
+            string[] surname = System.IO.File.ReadAllLines(@"C:\Users\Ata Yiğit TELLİ\Source\Repos\Challenges\Random Name Generator\Namelists\soyisimler.txt");
+            Console.WriteLine(name[rand.Next(0, name.Length)].ToLower() + " " + surname[rand.Next(0, surname.Length)].ToLower());
         }
     }
 }
